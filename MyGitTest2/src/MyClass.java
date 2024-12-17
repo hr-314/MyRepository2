00bcf83d2d1a2ad74ce9d7f92f212766465bb565
hyeli <hyeli@DESKTOP-IK1D4RU>
2024-12-18 00:21:28 +0900

    Change in master branch

--- MyGitTest2/src/MyClass.java
+++ MyGitTest2/src/MyClass.java
@@ -3,5 +3,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!!");
 		System.out.println("second version!");
+		System.out.println("My changed in master branch");
 	}
 }
